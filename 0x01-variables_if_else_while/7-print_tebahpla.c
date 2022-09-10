3dc18337e43d430f2315eb8f0c469db98db9023e
Ajosejoseph <ajosejosepholaitan@gmail.com>
2022-09-09 19:42:46 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+* main Entry point
+*
+*
+* Return : Always 0 (success)
+*
+*/
+
+int main(void)
+{
+char abc;
+for (abc = '2'; abc >= 'a'; abc++)
+putchar(abc);
+putchar('\n');
+return (0);
