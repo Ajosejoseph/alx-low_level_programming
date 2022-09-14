cfeb7d8742699b19474be40d63d1f03f50da5bef
Ajosejoseph <ajosejosepholaitan@gmail.com>
2022-09-13 22:39:01 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - print sign of a number
  * @n: the int to check
  * Return: 1 print + if n is greater than zero
- * 0 print 0 if n is zero
+ * 0 and print 0 if n is zero
  * -1 print - if n is less than zero
 */
 int print_sign(int n)
@@ -14,13 +14,11 @@ int print_sign(int n)
 	{
 		_putchar(43);
 		return (1);
-	}
-	else if (n == '0')
+	} else if (n == '0')
 	{
 		_putchar(48);
 		return (0);
-	}
-	else if (n < '0')
+	} else if (n < '0')
 	{
 		_putchar(45);
 	}
