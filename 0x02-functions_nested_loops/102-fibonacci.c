eeb16f80fac88ab2277dce02395d65f9bea1900f
Ajosejoseph <ajosejosepholaitan@gmail.com>
2022-09-14 21:31:01 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0
+*/
+
+int main(void)
+{
+	long a = 1;
+	long b = 2;
+	int n = 3;
+
+	printf("%li, ", a);
+	printf("%li, ", b);
+
+	while (n < 50)
+	{
+		long fib = a + b;
+
+		printf("%li. ", fib);
+		a = b;
+		b = fib;
+		n++;
+	}
+	while (n == 50)
+	{
+		long fib = a + b;
+
+		printf("%li\n", fib);
+		a = b;
+		b = fib;
+		n++;
+	}
+	return (0);
+}
