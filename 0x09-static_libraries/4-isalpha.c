c476d9432ea435790dbcfb0330aec356ca9ce453
Ajosejoseph <ajosejosepholaitan@gmail.com>
2022-09-30 17:46:57 -0700

    Task 0

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabetic character
+ * @c:character to check for
+ * Return: 1 if c is a letter,lowercase or uppercase else 0
+*/
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - print the absolute value of an integer
+ * @n: int to check for
+ * Return: absolute value of int
+*/
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
